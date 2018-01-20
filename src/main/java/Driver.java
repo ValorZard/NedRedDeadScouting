30c19e136e326ca09b399aed19b58f8569795867
Srayan Jana <sonicasianfan@gmail.com>
2018-01-20 15:18:56 -0800

    IT wORKSS!
    I can get data from MongoDB and put it to a JSON File

--- src/main/java/Driver.java
+++ src/main/java/Driver.java
@@ -3,15 +3,18 @@ import org.json.simple.JSONArray;
 import org.json.simple.JSONObject;
 import org.json.simple.parser.JSONParser;
 
+import java.io.FileWriter;
+import java.io.IOException;
 import java.util.*;
 import java.util.Map.*;
+
 public class Driver {
 
-    public static void main(String[] args){
+    public static void main(String[] args) {
         // parsing file "JSONExample.json"
         ArrayList<UnitOfData> unitsOfData = new ArrayList<UnitOfData>();
         ReadMit r = new ReadMit();
-        try{
+        try {
             Object obj = new JSONParser().parse(r.getFile());
 
             // typecasting obj to JSONObject
@@ -19,26 +22,50 @@ public class Driver {
 
             JSONArray ja = (JSONArray) jo.get("Data");
 
-            for(Object o : ja){
+            for (Object o : ja) {
                 Map map = (Map) o;
                 HashMap<String, String> hmap = new HashMap<String, String>();
-                for(Object m : map.entrySet()){
+                for (Object m : map.entrySet()) {
                     Entry e = (Entry) m;
-                    System.out.println(e.getKey()+" "+(String)(e.getValue()));
-                   hmap.put((String) (e.getKey()), (String) (e.getValue()));
+                    System.out.println(e.getKey() + " " + (String) (e.getValue()));
+                    hmap.put((String) (e.getKey()), (String) (e.getValue()));
                 }
                 UnitOfData u = new UnitOfData(hmap);
                 unitsOfData.add(u);
             }
             Calculator calc = new Calculator(unitsOfData);
-            while(true){
+            while (true) {
                 Scanner scan = new Scanner(System.in);
                 System.out.println("WHAT DO YOU WANT TO AVERAGE?????/*???????*/");
                 String s = scan.nextLine();
-                System.out.println(calc.average(s));
+                System.out.println(calc.average(s, "1458"));
+                System.out.println("Do you wanna do MongoDB");
+                scan = new Scanner(System.in);
+                s = scan.nextLine();
+                if (s.equalsIgnoreCase("yes")) {
+                    App app = new App();
+                    jo = app.run();
+                }
+                ja = (JSONArray) jo.get("Data");
+                System.out.println("----------------------------------------------------------");
+                try (FileWriter file = new FileWriter("test.json")) {
+
+                    file.write(((JSONObject) obj).toJSONString());
+                    file.flush();
+
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+                for (Object o : ja) {
+                    Map map = (Map) o;
+                    for (Object m : map.entrySet()) {
+                        Entry e = (Entry) m;
+                        System.out.println(e.getKey() + " " + e.getValue());
+                    }
+                }
             }
-    }
-        catch(Exception ex){
+
+        } catch (Exception ex) {
             System.out.println("It didnt work! REEEEEEEEEEE");
         }
 
---
+++ src/main/java/RedTieMongo.java
@@ -0,0 +1,112 @@
+import com.mongodb.MongoClient;
+import com.mongodb.client.FindIterable;
+import com.mongodb.client.MongoCollection;
+import com.mongodb.client.MongoDatabase;
+import org.bson.Document;
+import org.bson.json.JsonMode;
+import org.bson.json.JsonWriterSettings;
+import org.json.simple.JSONObject;
+import org.json.simple.parser.JSONParser;
+
+import java.util.Iterator;
+
+public class RedTieMongo {
+    private MongoClient mongo;
+    private MongoDatabase database;
+    private MongoCollection collection;
+    private String d, c;
+    public RedTieMongo(){
+        mongo = Helper.getInstance();
+        database = mongo.getDatabase("SERN");
+        collection = database.getCollection("Steins;Gate");
+
+    }
+
+    public RedTieMongo(String d, String c) {
+        this.d = d;
+        this.c = c;
+        mongo = Helper.getInstance();
+        this.database = mongo.getDatabase(d);
+        this.collection = database.getCollection(c);
+    }
+
+    public void readDatabase(){
+
+        database = mongo.getDatabase(d);
+        collection =  database.getCollection(c);
+        System.out.println("Collection myCollection selected successfully");
+        // Retrieving the documents after updation
+        // Getting the iterable object
+        FindIterable<Document> iterDoc = collection.find();
+        int i = 1;
+
+        // Getting the iterator
+        Iterator it = iterDoc.iterator();
+
+        while (it.hasNext()) {
+            System.out.println(it.next());
+            i++;
+        }
+    }
+
+    public JSONObject databaseJSON(){
+
+        database = mongo.getDatabase(d);
+        collection =  database.getCollection(c);
+        System.out.println("CONVERSION START");
+        // Retrieving the documents after updation
+        // Getting the iterable object
+        FindIterable<Document> iterDoc = collection.find();
+        int i = 1;
+        int c = 0;
+        // Getting the iterator
+        Iterator it = iterDoc.iterator();
+        String s = "{\n" +
+                "\t\"Data\": [\n";
+        while (it.hasNext()) {
+            Document doc = (Document) it.next();
+            c++;
+        }
+        it = iterDoc.iterator();
+        while (it.hasNext()) {
+            JsonWriterSettings writerSettings = new JsonWriterSettings(JsonMode.SHELL, true);
+            Document doc = (Document) it.next();
+            s+=doc.toJson(writerSettings);
+            if(!(i == c)) {
+                s += ",";
+            }
+            s+="\n";
+            i++;
+        }
+        s+="]\n" +
+                "}";
+        try {
+            Object obj = new JSONParser().parse(s);
+            System.out.println(s);
+            return (JSONObject) obj;
+        }catch(Exception ex){
+            System.out.println("REEEE");
+        }
+        return null;
+    }
+
+    public MongoClient getMongo() {
+        return mongo;
+    }
+
+    public MongoDatabase getDatabase() {
+        return database;
+    }
+
+    public void setDatabase(String db) {
+        this.database  = mongo.getDatabase(db);
+    }
+
+    public MongoCollection getCollection() {
+        return collection;
+    }
+
+    public void setCollection(String c) {
+        this.collection = database.getCollection(c);
+    }
+}
